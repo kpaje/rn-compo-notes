693c1960277141a6761e3ce1a3e0b679db7bdaa2
kpcruz <kelvinmembership@gmail.com>
2019-08-13 12:46:13 -0700

    button component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-	return <Text style={styles.text}>HomeScreen</Text>;
+	return (
+		<View>
+			<Text style={styles.text}>HomeScreen</Text>
+			<Button title="Press me" onPress={() => console.log("Button Pressed")} />
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
