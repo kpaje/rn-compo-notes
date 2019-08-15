d12d146295129fec4fbbb39729c46ba797c52387
kpcruz <kelvinmembership@gmail.com>
2019-08-15 09:37:55 -0700

    refactored to ternary operator and validators for >255 and <0  rgb colors

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -8,14 +8,20 @@ const COLOR_INCREMENT = 30;
 const reducer = (state, action) => {
 	// state === {red: number, green: number, blue: number}
 	// action === {colorToChange: 'red' || 'green || 'blue, amount: 15 || -15}
-	console.log(action);
 	switch (action.colorToChange) {
 		case "red":
-			return { ...state, red: state.red + action.amount };
+			return state.red + action.amount > 255 || state.red + action.amount < 0
+				? state
+				: { ...state, red: state.red + action.amount };
 		case "green":
-			return { ...state, green: state.green + action.amount };
+			return state.green + action.amount > 255 ||
+				state.green + action.amount < 0
+				? state
+				: { ...state, green: state.green + action.amount };
 		case "blue":
-			return { ...state, blue: state.blue + action.amount };
+			return state.blue + action.amount > 255 || state.blue + action.amount < 0
+				? state
+				: { ...state, blue: state.blue + action.amount };
 		default:
 			return;
 	}
