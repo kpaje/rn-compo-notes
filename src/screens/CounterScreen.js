0c2db38c6f3e7ae38124f6e4a17d1e30cda50390
kpcruz <kelvinmembership@gmail.com>
2019-08-15 10:02:51 -0700

    went overkill and refactored counter component with reducer. Good practice!

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,14 +1,35 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 
+//action means howToChangeState
+const reducer = (state, action) => {
+	//state === {count: number}
+	//action === {type: 'increment' || 'decrement', payload: 1 || -1}
+	switch (action.type) {
+		case "increment":
+			return { ...state, count: state.count + action.payload };
+		case "decrement":
+			return { ...state, count: state.count - action.payload };
+		default:
+			return;
+	}
+};
+
 const CounterScreen = ({}) => {
-	const [counter, setCounter] = useState(0);
+	const [state, dispatch] = useReducer(reducer, { count: 0 });
+	const { count } = state;
 
 	return (
 		<View>
-			<Button title="Increase" onPress={() => setCounter(counter + 1)} />
-			<Button title="Decrease" onPress={() => setCounter(counter - 1)} />
-			<Text>Current Count: {counter}</Text>
+			<Button
+				title="Increase"
+				onPress={() => dispatch({ type: "increment", payload: 1 })}
+			/>
+			<Button
+				title="Decrease"
+				onPress={() => dispatch({ type: "decrement", payload: 1 })}
+			/>
+			<Text>Current Count: {count}</Text>
 		</View>
 	);
 };
